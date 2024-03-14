7ab40f8c75e1ffc0fd6b62c58c6f94b0a522fd3b
Stark-Akshay <akshaymanoj.dms@gmail.com>
2024-03-14 08:10:55 +0000

    image warnings corrected

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
 
     /*Write out tailwind css from mobile standard then build it up*/
     <div className='banner-parent flex justify-center flex-col-reverse mt-10 items-center sm:justify-around md:flex-row'>
-        <Image className='banner-child1 w-full h-auto max-w-[250px] sm:max-w-[350px] md:max-w-[450px] lg:max-w-[500px]' src={BannerCraneImage}/>
+        <Image className='banner-child1 w-full h-auto max-w-[250px] sm:max-w-[350px] md:max-w-[450px] lg:max-w-[500px]' src={BannerCraneImage} alt='banner'/>
         <div className='banner-child2 text-content flex flex-col px-5 w-full justify-around items-center  md:space-y-5 text-center'>
             <p className='text-[4rem] leading-[4.5rem] md:text-[5rem] font-m-sans text-primary font-medium'>Entrust Shipping</p>
             <p className='text-lg font-m-sans text-secondary font-semibold'>Your One-Stop Shop for Global Shipping and Logistics</p>
